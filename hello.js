569092f367101098195033478275edc4f157915f
Vitor Silva Delfino <me@vitordelfino.dev>
2021-11-25 16:02:14 -0300

    test

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
+const fs = require('fs');
+
+const json = fs.readFileSync('./users.json')
+
 console.log('Hello', process.env.USERNAME);
-console.log('json', JSON.parse(process.env.JSON_DATA))
+console.log('json', json)
